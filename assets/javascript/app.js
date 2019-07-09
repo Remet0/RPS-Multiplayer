04c3419d82a3db11705c71de4e58c948dce45976
Remeto <remetojohn@gmail.com>
2019-07-09 10:21:21 -0700

    Linked files and added jquery and firebase setups. Added reset code to css

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAfrrOFijzp3rQvvs23c8MIHSlDpTZYy8w",
+    authDomain: "rps-multiplayer-jr.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-jr.firebaseio.com",
+    projectId: "rps-multiplayer-jr",
+    storageBucket: "",
+    messagingSenderId: "921677088064",
+    appId: "1:921677088064:web:a121f9def6596247"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
